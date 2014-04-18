0ab21aca11b36805d0a179f687eaade7ec769c28
varchenko <p.varchenko@eis.ru>
2014-04-18 17:45:06 +0400

    create db file

---
+++ database/create_db.sql
@@ -0,0 +1,12 @@
+﻿-- Database: dnevnik
+
+-- DROP DATABASE dnevnik;
+
+CREATE DATABASE dnevnik
+  WITH OWNER = admin
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Russian_Russia.1251'
+       LC_CTYPE = 'Russian_Russia.1251'
+       CONNECTION LIMIT = -1;
+
